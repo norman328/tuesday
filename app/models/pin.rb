e47f1f362bc64ff513fadd6287c14581cc28543e
Jeremy Norman <norman328@yahoo.com>
2013-04-29 15:11:56 -0400

    Add Pins Scaffold

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,9 @@
+
+
 class Pin < ActiveRecord::Base
   attr_accessible :description
+  validates :description, presence: true #other validations can be found on ruby on railsite such as legnth just sep by comma
+  
+
+    
 end
